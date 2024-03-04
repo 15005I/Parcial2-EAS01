f4fdaafeccc65a668624935e52f30241e3915687
Iker Herrera <15005@portalcedi.edu.mx>
2024-03-04 14:53:11 -0600

    Sumativa2

--- jugadores.cpp
+++ jugadores.cpp
@@ -1,64 +1,81 @@
 #include <iostream>
 #include <string>
-#include <cstdlib>  // Función rand().
-#include <ctime>    // Función time() para generar una nueva seed aleatoria.
+#include <cstdlib> // Función rand().
+#include <ctime>   // Función time() para generar una nueva seed aleatoria.
 using namespace std;
 
 #define log(x) cout << x << endl;
 
-class Jugador{
-    /*
-        Agrega propiedades para guardar la vida(número entero), ataque(número entero) y nombre del jugador.
-        Agrega una propiedad para guardar el jugador que lo atacó por última vez.
-    */
-    
-    Jugador(); 
-    Jugador(/* agrega parámetros para asignar hp, ataque y nombre */); 
-    
-    void atacar(/* agregar parámetros para atacar a otro jugador*/); 
+class Jugador
+{
+public:
+    int vida;
+    int ataque;
+    string nombre;
+    Jugador *atacadoPor;
+    Jugador();
+    Jugador(int v, int atk, string n);
+
+    void atacar(Jugador* jugador);
 };
 
 int main()
 {
-    srand(time(0));  // Seed para generar valores aleatorios
-    
+    srand(time(0)); // Seed para generar valores aleatorios
+    Jugador j1(100, 10, "Juan"), j2(200, 20, "PedroHD");
+    while(j1.vida>0 && j2.vida>0){
+        j1.atacar(&j2);
+        if(j1.vida>0 && j2.vida>0){
+            j2.atacar(&j1);
+        }else{
+            break;
+        }
+    }
+    if(j1.vida>0 && j2.vida<=0){
+        cout << j1.nombre << " es el ganador" << endl;
+    }else{
+        cout << j2.nombre << " es el ganador" << endl;
+    }
     /*
-        Crear al menos 2 jugadores con diferentes nombres, vida y ataque. 
+        Crear al menos 2 jugadores con diferentes nombres, vida y ataque.
         Los jugadores deben atacarse entre sí, tomando turnos, hasta que uno pierda toda su vida.
-        En cuanto la vida del jugador se reduzca a 0 el programa muestra al ganador y se termina. 
+        En cuanto la vida del jugador se reduzca a 0 el programa muestra al ganador y se termina.
         No importa quien ataca primero.
     */
 
     return 0;
 }
 
-Jugador::Jugador(){
-    /*
-        Asignar 100 a hp y 5 a attack como valores default.
-        Asignar 'No definido' al nombre como default.
-    */
+Jugador::Jugador()
+{
+    vida = 100;
+    ataque = 5;
+    nombre = "No definido";
 }
 
-Jugador::Jugador(/* Agrega parámetros para asignar hp, ataque y nombre*/){
-    /*
-        Asignar los parámetros recibidos a las propiedades del objeto.
-        No se pueden asignar valores negativos a hp y attack.
-        Máximo 200 hp y 20 attack
-    */
+Jugador::Jugador(int v, int atk, string n)
+{
+    if(v>0 && atk>0 && v<201 && atk<21){
+        vida = v;
+        ataque = atk;
+    }else if(v<1 && atk<1){
+        vida = 1;
+        ataque = 1;
+    }else if(v>200 && atk>20){
+        vida = 200;
+        ataque = 20;
+    }
+    nombre = n;
 }
 
-void Jugador::atacar(/* Agregar parámetros para atacar a otro Jugador */){
-    /*
-        El jugador atacado(el que recibes como parámetro) pierde HP igual al ataque del jugador que ataca.
-        Existe un 20% de probabilidad de que el ataque falle y no haga daño.
-        Se debe mostrar lo que pasó en cada ataque:
-        nombre1 atacó a nombre2 e hizo 5 de daño, vida restante de nombre2 = 95
-        nombre2 atacó a nombre1 pero fallo, vida restante de nombre1 = 100
-        El jugador que atacó debe quedar guardar en la propiedad atacadoPor del jugador atacado.
-    */
-    /*
-    Nota:
-        int x = rand() % 1000; asigna un valor aleatorio entre 0 y 999 a x;
-        Puedes usar esto para la probabilidad del ataque.
-    */
+void Jugador::atacar(Jugador* jugador)
+{
+    int x = rand() % 10;
+    if(x>1){
+        jugador->vida -= this->ataque;
+        cout << this->nombre << " ataco a " << jugador->nombre << "e hizo " << ataque << " de daño, vida restante de " << jugador->nombre << " = " << jugador->vida << endl;
+    }else{
+        cout << this->nombre << " ataco a " << jugador->nombre << " pero fallo, vida restante de " << jugador->nombre << " = " << jugador->vida << endl;
+    }
+    jugador->atacadoPor = this;
 }
